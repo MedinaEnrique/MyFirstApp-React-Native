abcc080f5621ec33bbc58d5a492e837ceaa3c426
Enrique Medina <emedina@roshka.com>
2024-06-02 21:34:04 -0400

    chore: Add react-native-paper and react-native-safe-area-context dependencies, and update App.tsx and CounterScreen.tsx to use them

--- App.tsx
+++ App.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import {View} from 'react-native';
 import HelloWorldScreen from './src/presentation/screens/HelloWorldScreen';
 import CounterScreen from './src/presentation/screens/CounterScreen';
+import {PaperProvider} from 'react-native-paper';
 
 function App() {
   return (
-    <View style={{flex: 1}}>
-      <HelloWorldScreen name="David" />
-      <CounterScreen />
-    </View>
+    <PaperProvider>
+      <View style={{flex: 1}}>
+        <HelloWorldScreen name="David" />
+        <CounterScreen />
+      </View>
+    </PaperProvider>
   );
 }
 export default App;
--- src/presentation/screens/CounterScreen.tsx
+++ src/presentation/screens/CounterScreen.tsx
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
-import {Pressable, StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
+import {Button} from 'react-native-paper';
+import {FAB} from 'react-native-paper';
+import {globalStyles as styles} from '../theme/global.styles';
 
 const CounterScreen = () => {
   const [counter, setCounter] = useState(0);
@@ -7,70 +10,19 @@ const CounterScreen = () => {
   const decrement = () => setCounter(counter - 1);
   const reset = () => setCounter(0);
   return (
-    <View style={styles.container}>
-      <Pressable
-        onPress={decrement}
-        style={({pressed}) => [styles.button, pressed && styles.buttonPressed]}>
-        <Text style={styles.textButton}>-</Text>
-      </Pressable>
-      <View style={styles.subContainer}>
-        <Text style={styles.title}> {counter} </Text>
-        <Pressable
-          onPress={reset}
-          style={({pressed}) => [
-            styles.button,
-            pressed && styles.buttonPressed,
-          ]}>
-          <Text style={styles.textButton}>Reestablecer</Text>
-        </Pressable>
+    <View style={styles.centerContainer}>
+      <View style={styles.subCenterContainer}>
+        <Button mode="contained" onPress={decrement}>
+          -
+        </Button>
+        <Button mode="contained" onPress={increment}>
+          +
+        </Button>
       </View>
-      <Pressable
-        onPress={increment}
-        style={({pressed}) => [styles.button, pressed && styles.buttonPressed]}>
-        <Text style={styles.textButton}>+</Text>
-      </Pressable>
+      <Text style={styles.title}> {counter} </Text>
+      <FAB style={styles.fab} onPress={reset} label="Reestablecer" />
     </View>
   );
 };
 
 export default CounterScreen;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#006633',
-  },
-  subContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 45,
-    padding: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  button: {
-    padding: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    margin: 10,
-    backgroundColor: 'white',
-    borderRadius: 10,
-  },
-  buttonPressed: {
-    padding: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    margin: 10,
-    backgroundColor: '#80ff80',
-    borderRadius: 10,
-  },
-  textButton: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#006633',
-  },
-});
